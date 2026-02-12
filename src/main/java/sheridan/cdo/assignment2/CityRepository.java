41738f4112a3fd8caa05ac198e0a552eeaf063f8
Andrew <andrewhuecdo@gmail.com>
2026-02-12 01:04:39 -0500

    added extra programing comments to explain what things do
    To access use
    http://localhost:8080/

--- src/main/java/sheridan/cdo/assignment2/CityRepository.java
+++ src/main/java/sheridan/cdo/assignment2/CityRepository.java
@@ -1,3 +1,8 @@
+//Andrew Do
+//Programming Assignment 2
+//Prog27545 Web Application Design and Implementation
+//Last edited 2026-02-12
+
 package sheridan.cdo.assignment2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,9 +13,18 @@ import java.util.List;
 @RepositoryRestResource
 public interface CityRepository extends JpaRepository<City, String> {
 
+    // Searches the H2 database for cities that match the given name and outputs the result to the resfful API
     List<City> findByName(String name);
+
+    // Searches the H2 database for cities that match the given country and outputs the result to the resfful API
     List<City> findByCountry(String country);
+
+    // Searches the H2 database for cities with a population greater than the given value and outputs the result to the resfful API
     List<City> findByPopulationGreaterThan(long population);
+
+    // Searches the H2 database for cities with a population less than the given value and outputs the result to the resfful API
     List<City> findByPopulationLessThan(long population);
+
+    // Searches the H2 database for cities with a population between the two given values and outputs the result to the resfful API
     List<City> findByPopulationBetween(long low, long high);
 }
--- src/test/java/sheridan/cdo/assignment2/ApplicationTests.java
+++ src/test/java/sheridan/cdo/assignment2/ApplicationTests.java
@@ -1,3 +1,7 @@
+//Andrew Do
+//Programming Assignment 2
+//Prog27545 Web Application Design and Implementation
+//Last edited 2026-02-12
 package sheridan.cdo.assignment2;
 
 import org.junit.jupiter.api.Test;
